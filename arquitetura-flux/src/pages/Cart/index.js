378398b0c1178d737908041a1622ff67d60df1ec
irailtonreis <irailton.oliveira91@gmail.com>
2020-04-17 14:56:16 -0300

    feat: arquitetura flux: add server products

--- arquitetura-flux/src/pages/Cart/index.js
+++ arquitetura-flux/src/pages/Cart/index.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React  from "react";
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
+              alt="Tênis muito' massa"/>
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <strong>R$129,000</strong>
+            </td>
+            <td>
+              <div>
+              <button type="button">
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={1}/>
+              <button type="button">
+                <MdAddCircleOutline size={20} color="#7159c1"/>
+              </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$258,80</strong>
+              </td>
+              <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1"/>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1.920,00</strong>
+
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- arquitetura-flux/src/pages/Home/index.js
+++ arquitetura-flux/src/pages/Home/index.js
@@ -1,110 +1,52 @@
-import React from "react";
+import React, { Component} from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 import { ProductList } from "./styles";
 
-export default function Home() {
+import api from '../../services/api';
+import {formatPrice} from '../../utils/format';
+
+export default class Home extends Component {
+
+  state = {
+    products: [],
+
+  }
+
+  async componentDidMount(){
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+
+    this.setState({products: data})
+  }
+ render(){
+
+  const { products} = this.state;
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênins muito legal</strong>
-        <span>R$129,00</span>
+      {products.map(product => (
+         <li key={product.id}>
+         <img
+           src={product.image}
+           alt={product.title}
+         />
+         <strong>{product.title}</strong>
+      <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={36} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+         <button type="button">
+           <div>
+             <MdAddShoppingCart size={36} color="#FFF" />3
+           </div>
+           <span>ADICIONAR AO CARRINHO</span>
+         </button>
+       </li>
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênins muito legal</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={36} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênins muito legal</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={36} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênins muito legal</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={36} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênins muito legal</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={36} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-courtjam-bounce-masculino/26/COL-6266-026/COL-6266-026_zoom1.jpg?ts=1584552361&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênins muito legal</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={36} color="#FFF" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+      ))}
 
     </ProductList>
   );
+ }
 }
