bc50f94249f5fc2c4eb360e0d4dc06b3a3057244
irailtonreis <irailton.oliveira91@gmail.com>
2020-04-20 16:51:53 -0300

    feat: aquiterura-flux : add redux reactotron

--- arquitetura-flux/src/pages/Home/index.js
+++ arquitetura-flux/src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component} from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 import { ProductList } from "./styles";
+import { connect } from 'react-redux';
 
 import api from '../../services/api';
 import {formatPrice} from '../../utils/format';
 
-export default class Home extends Component {
+ class Home extends Component {
 
   state = {
     products: [],
@@ -22,6 +23,17 @@ export default class Home extends Component {
 
     this.setState({products: data})
   }
+
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
+
  render(){
 
   const { products} = this.state;
@@ -36,7 +48,9 @@ export default class Home extends Component {
          <strong>{product.title}</strong>
       <span>{product.priceFormatted}</span>
 
-         <button type="button">
+         <button type="button"
+          onClick={()=> this.handleAddProduct(product)}
+         >
            <div>
              <MdAddShoppingCart size={36} color="#FFF" />3
            </div>
@@ -50,3 +64,6 @@ export default class Home extends Component {
   );
  }
 }
+
+
+export default connect()(Home);
