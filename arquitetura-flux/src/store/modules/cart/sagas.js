0cd30fee18b472d4fbed5b80075924ca816a82e3
irailtonreis <irailton.oliveira91@gmail.com>
2020-04-27 11:04:45 -0300

    arquitetura-flux: update actions sagas

--- arquitetura-flux/src/store/modules/cart/sagas.js
+++ arquitetura-flux/src/store/modules/cart/sagas.js
@@ -1,11 +1,37 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSucess } from './actions';
+import { formatPrice } from '../../../utils/format';
+import { addToCartSucess, updateAmount } from './actions';
 function* addToCart({id}){
-const response = yield call(api.get, `/products/${id}`);
+const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
+const stock = yield call(api.get, `/stock/${id}`);
+const stockAmount = stock.data.amount;
+ const currentAmount = productExists ? productExists.amount : 0;
 
-yield put(addToCartSucess(response.data))
+const amount = currentAmount + 1;
+
+if(amount > stockAmount){
+  console.tron.warn("Error");
+  return;
+}
+
+if(productExists){
+
+  yield put(updateAmount(id, amount));
+
+}else{
+  const response = yield call(api.get, `/products/${id}`);
+
+  const data = {
+    ...response.data,
+    amount: 1,
+    priceFormatted: formatPrice(response.data.price)
+  }
+  yield put(addToCartSucess(data))
+}
 
 }
 
